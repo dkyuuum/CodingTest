3237a55c64760436cf4a73da0f9bc5d70a6be138
Kyx <dkyuuum@gmail.com>
2025-02-03 20:08:32 +0900

    [level 2] Title: 올바른 괄호, Time: 18.59 ms, Memory: 55 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,19 +2,16 @@ import java.util.*;
 
 class Solution {
     boolean solution(String s) {
-        boolean answer = true;
-        char [] arr = s.toCharArray();
-        Stack<Integer> stack = new Stack<>();
+        Stack<Character> stack = new Stack<>();
 
-        for(char cur : arr)
-        {
-           if(cur == '('){
-               stack.push(0); 
-               continue;
-           }
-           if(stack.isEmpty() || stack.peek() == 1) return false;
-           stack.pop();
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(c);
+            } else { // c == ')'
+                if (stack.isEmpty()) return false;
+                stack.pop();
+            }
         }
-        return stack.isEmpty() == true ? true : false;
+        return stack.isEmpty();
     }
 }
