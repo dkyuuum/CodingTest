827610cd6ec36840958832055ca92e4c0b2c0c7c
Kyumin <dkyuuum@gmail.com>
2025-04-24 10:17:33 +0900

    [Silver III] Title: 모든 순열, Time: 1728 ms, Memory: 66684 KB -BaekjoonHub

--- 백준/Silver/10974. 모든 순열/모든 순열.java
+++ 백준/Silver/10974. 모든 순열/모든 순열.java
@@ -8,20 +8,20 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        int[] result = new int[n];
         boolean[] visited = new boolean[n];
 
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = i+1;
         }
-        permutation(arr, result, visited, 0, n, n);
+        permutation(arr, visited, 0, n);
+//        permutationByBitMasking(arr);
     }
 
     // 사전순 정렬, n개 중 r개 뽑기
-    public static void permutation(int[] arr, int[] result, boolean[] visited, int depth, int n, int r) {
-        if (depth == r) {
-            for (int res : result) {
+    public static void permutation(int[] arr, boolean[] visited, int depth, int n) {
+        if (depth == n) {
+            for (int res : arr) {
                 System.out.print(res + " ");
             }
             System.out.println();
@@ -30,12 +30,11 @@ public class Main {
 
         for (int i=0; i<n; i++) {
             if (!visited[i]) { // 아직 방문하지 않았다면
-                visited[i] = true; // 방문 체크
-                result[depth] = arr[i];
-                permutation(arr, result, visited, depth+1, n, r);
-                visited[i] = false;
+                visited[i] = true;
+                arr[depth] = i+1;
+                permutation(arr, visited, depth+1, n);
+                visited[i] = false; // 백트래킹
             }
         }
     }
-
 }
