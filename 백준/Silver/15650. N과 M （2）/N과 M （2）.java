505d3fae9b86791287c1dbecbe31ed8158b3f9eb
Kyumin <dkyuuum@gmail.com>
2025-04-27 23:05:50 +0900

    [Silver III] Title: N과 M (2), Time: 68 ms, Memory: 11548 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -11,25 +11,23 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        int[] arr = new int[N];
+        int[] arr = new int[M];
 
-        combination(arr, 0, 1, 0, N, M);
+        combination(arr, 1, 0, N, M);
     }
 
-    public static void combination(int[] arr, int flag, int num, int depth, int n, int r) {
-        if(depth == r) {
-            for (int i=0; i<r; i++) {
-                System.out.print(arr[i]+" ");
+    public static void combination(int[] arr, int start, int depth, int n, int r) {
+        if (depth == r) {
+            for (int i = 0; i < r; i++) {
+                System.out.print(arr[i] + " ");
             }
             System.out.println();
             return;
-        } else {
-            for (int i=num; i<=n; i++) {
-                if ((flag & (1 << i)) == 0) {
-                    arr[depth] = i;
-                    combination(arr, flag|(1 << i), i+1, depth+1, n, r);
-                }
-            }
+        }
+
+        for (int i = start; i <= n; i++) {
+            arr[depth] = i;
+            combination(arr, i + 1, depth + 1, n, r);
         }
     }
 }
