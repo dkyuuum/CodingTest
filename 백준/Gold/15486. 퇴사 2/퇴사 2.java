14117a2a63e95b29ca03256feb3110bc5c4403b2
Kyumin <dkyuuum@gmail.com>
2025-05-12 00:16:25 +0900

    [Gold V] Title: 퇴사 2, Time: 620 ms, Memory: 314368 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] date = new int[N+1];
+        int[] money = new int[N+1];
+        int[] dp = new int[N+2];
+
+        for (int i=1; i<=N; i++) {
+           StringTokenizer st = new StringTokenizer(br.readLine());
+            date[i] = Integer.parseInt(st.nextToken());
+            money[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i=1; i<=N; i++) {
+            // i일까지의 최대 수익을 다음 날로 전달
+            dp[i + 1] = Math.max(dp[i + 1], dp[i]);
+
+            // 상담 기간이 남아있는 퇴사 날짜보다 길다면
+            if (date[i]+i > N+1) {
+                // 상담 불가능
+                continue;
+            } else {
+                // 상담 가능
+                dp[i + date[i]] = Math.max(dp[i + date[i]], dp[i] + money[i]);
+            }
+        }
+        System.out.println(Math.max(dp[N], dp[N + 1]));
+    }
+}
