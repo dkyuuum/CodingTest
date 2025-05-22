688dc54ff3ccd434825a57c4364b7556c660d687
Kyumin <dkyuuum@gmail.com>
2025-05-22 13:27:42 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -1,11 +1,19 @@
-select a.ANIMAL_ID, a.ANIMAL_TYPE, a.NAME	
-from ANIMAL_INS a
-    join ANIMAL_OUTS b
-    on a.ANIMAL_ID = b.ANIMAL_ID
-where 
-    a.SEX_UPON_INTAKE like "Intact%"
-    and
-    (b.SEX_UPON_OUTCOME like "Neutered%"
-    or
-    b.SEX_UPON_OUTCOME like "Spayed%")
-order by a.ANIMAL_ID
+# select a.ANIMAL_ID, a.ANIMAL_TYPE, a.NAME	
+# from ANIMAL_INS a
+#     join ANIMAL_OUTS b
+#     on a.ANIMAL_ID = b.ANIMAL_ID
+# where 
+#     a.SEX_UPON_INTAKE like "Intact%"
+#     and
+#     (b.SEX_UPON_OUTCOME like "Neutered%"
+#     or
+#     b.SEX_UPON_OUTCOME like "Spayed%")
+# order by a.ANIMAL_ID
+
+SELECT a.ANIMAL_ID, a.ANIMAL_TYPE, a.NAME
+FROM ANIMAL_INS a
+JOIN ANIMAL_OUTS b ON a.ANIMAL_ID = b.ANIMAL_ID
+WHERE 
+    INSTR(a.SEX_UPON_INTAKE, 'Intact') > 0
+    AND b.SEX_UPON_OUTCOME REGEXP 'Neutered|Spayed'
+ORDER BY a.ANIMAL_ID;
