9e092e6cc345c6bd2732078a83617b18c1e9e4e4
Kyumin <dkyuuum@gmail.com>
2025-05-30 09:57:47 +0900

    [level 1] Title: 신고 결과 받기, Time: 216.99 ms, Memory: 184 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -1,39 +1,41 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
         int[] answer = new int[id_list.length];
         
-        // 중복되는 신고 없애기
-        Set<String> reportSet = new HashSet<>(Arrays.asList(report));
-        // System.out.println(reportSet);
+        // 중복 신고 제거
+        Set<String> set = new HashSet<>(Arrays.asList(report));
         
-        // 신고 받은 횟수
+        // 각 유저별 신고당한 횟수
         Map<String, Integer> reportCount = new HashMap<>();
-        Map<String, List<String>> iReportWho = new HashMap<>();
         
-        for (String s : reportSet){
-            String[] part = s.split(" ");
-            String from = part[0]; // 신고한 사람
-            String to = part[1]; // 신고 당한 사람
+        // 신고 from, to 
+        Map<String, List<String>> reportList = new HashMap<>();
+        
+        for (String s: set) {
+            String[] array = s.split(" ");
+            String from = array[0];
+            String to = array[1];
             
-            reportCount.put(to, reportCount.getOrDefault(to, 0) + 1);
+            reportCount.put(to, reportCount.getOrDefault(to, 0)+1);
             
-            if(!iReportWho.containsKey(from)){
-                iReportWho.put(from, new ArrayList<>());
+            if (!reportList.containsKey(from)) {
+                reportList.put(from, new ArrayList<>());
             }
-            iReportWho.get(from).add(to);
+            reportList.get(from).add(to);
         }
         
-        // K번 이상 신고받은 사람들 찾기
-        for(int i = 0; i < id_list.length; i++){
-            String reporter = id_list[i];
-            for(String reportee: iReportWho.getOrDefault(reporter, new ArrayList<>())){
-                if(reportCount.get(reportee) >= k){
-                    answer[i] += 1;
+        // k번 이상 신고당한 유저
+        for (int i=0; i<id_list.length; i++) {
+            String id = id_list[i];
+            
+            for (String reported: reportList.getOrDefault(id, new ArrayList<>())) {
+                if (reportCount.get(reported) >= k) {
+                       answer[i]+=1;
                 }
             }
         }
-            // 제재 받은 사람을 신고한 사람한테 알림주기 answer[i]++;
         return answer;
     }
 }
