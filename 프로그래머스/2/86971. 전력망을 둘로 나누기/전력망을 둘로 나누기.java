0a5ec3633a83fd7ebe47f07c24168eae044be611
Kyumin <dkyuuum@gmail.com>
2025-05-30 11:44:47 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 9.83 ms, Memory: 88.2 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -9,43 +9,45 @@ class Solution {
             
             for (int j=0; j<=n; j++) {
                 graph.add(new ArrayList<>());
-            }            
+            }
             
-           for (int j = 0; j < wires.length; j++) {
-                if (i == j) continue; // i번째 간선은 끊는다
+            for (int j=0; j<wires.length; j++) {
+                if (i == j) continue;
                 int a = wires[j][0];
                 int b = wires[j][1];
+                
                 graph.get(a).add(b);
                 graph.get(b).add(a);
             }
-
             
             int count = bfs(n, wires[i][0], graph);
             int diff = Math.abs(n - count - count);
-            answer = Math.min(answer, diff);
+            answer = Math.min(diff, answer);
         }
+        
         return answer;
     }
     
     public int bfs(int n, int start, List<List<Integer>> graph) {
-        Queue<Integer> queue = new LinkedList<>();
         boolean[] visited = new boolean[n+1];
-        int count = 1;
+        Queue<Integer> queue = new LinkedList<>();
         
         queue.offer(start);
         visited[start] = true;
         
-        while(!queue.isEmpty()) {
-            int q = queue.poll();
+        int count = 1;
+        while (!queue.isEmpty()) {
+            int cur = queue.remove();
             
-            for (int now: graph.get(q)) {
-                if (!visited[now]) {
-                    visited[now] = true;
-                    queue.offer(now);
+            for (int next: graph.get(cur)) {
+                if (!visited[next]) {
+                    visited[next] = true;
+                    queue.offer(next);
                     count++;
                 }
             }
         }
+        
         return count;
     }
 }
