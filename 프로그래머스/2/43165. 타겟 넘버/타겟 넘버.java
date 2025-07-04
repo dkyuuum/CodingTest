9e1afd653db3696019d5442be0b3c472954a901d
Kyumin <dkyuuum@gmail.com>
2025-07-04 13:31:33 +0900

    [level 2] Title: 타겟 넘버, Time: 8.27 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,17 +1,19 @@
 class Solution {
-    public int count = 0;
+    int answer = 0;
     public int solution(int[] numbers, int target) {
         backtracking(numbers, target, 0, 0);
-        return count;
+        
+        return answer;
     }
     
-    public void backtracking(int[] numbers, int target, int index, int make) {
+    public void backtracking(int[] numbers, int target, int make, int index) {
         if (numbers.length == index) {
-            if (target == make) count++;
+            if (make == target)
+                answer++;
             return;
         }
         
-        backtracking(numbers, target, index+1, make+numbers[index]);
-        backtracking(numbers, target, index+1, make-numbers[index]);
+        backtracking(numbers, target, make+numbers[index], index+1);
+        backtracking(numbers, target, make-numbers[index], index+1);
     }
 }
