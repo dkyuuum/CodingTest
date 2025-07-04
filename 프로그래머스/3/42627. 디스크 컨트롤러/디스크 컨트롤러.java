19fd2caf5181c33fdf0b0228c61bfee26e146e14
Kyumin <dkyuuum@gmail.com>
2025-07-04 10:57:37 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 4.16 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class Solution {
+    public int solution(int[][] jobs) {
+        Arrays.sort(jobs, (j1, j2) -> j1[0] - j2[0]);
+        
+        PriorityQueue<int[]> pq = new PriorityQueue<>((j1, j2) -> j1[1] - j2[1]);
+        
+        // 현재 시간, 완료된 작업 수, 총 작업 시간, 인덱스
+        int current = 0;
+        int completed = 0;
+        int total = 0;
+        int index = 0;
+        int len = jobs.length;
+        
+        while (len > completed) {
+            while (len > index && current >= jobs[index][0]) {
+                pq.add(jobs[index]);
+                index++;
+            }
+            
+            if (!pq.isEmpty()) {
+                int[] job = pq.remove();
+                
+                current += job[1];
+                total += current - job[0];
+                completed++;
+            } else {
+                current = jobs[index][0];
+            }
+        }
+        return total / len;
+    }
+}
