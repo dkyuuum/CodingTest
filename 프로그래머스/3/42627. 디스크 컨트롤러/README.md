f088ecfefca6751e7996479ddae2ef7ff6f68e4a
Kyumin <dkyuuum@gmail.com>
2025-07-04 13:09:40 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 3.45 ms, Memory: 93.2 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/README.md
+++ 프로그래머스/3/42627. 디스크 컨트롤러/README.md
@@ -1,10 +1,10 @@
 # [level 3] 디스크 컨트롤러 - 42627 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627) 
 
 ### 성능 요약
 
-메모리: 73.1 MB, 시간: 4.16 ms
+메모리: 93.2 MB, 시간: 3.45 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 04일 10:57:35
+2025년 07월 04일 13:09:38
 
 ### 문제 설명
 
--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -2,13 +2,12 @@ import java.util.*;
 class Solution {
     public int solution(int[][] jobs) {
         Arrays.sort(jobs, (j1, j2) -> j1[0] - j2[0]);
-        
         PriorityQueue<int[]> pq = new PriorityQueue<>((j1, j2) -> j1[1] - j2[1]);
         
-        // 현재 시간, 완료된 작업 수, 총 작업 시간, 인덱스
+        // 현재 시간, 총 작업 시간, 완료된 작업수, 인덱스
+        int total = 0;
         int current = 0;
         int completed = 0;
-        int total = 0;
         int index = 0;
         int len = jobs.length;
         
@@ -27,7 +26,7 @@ class Solution {
             } else {
                 current = jobs[index][0];
             }
-        }
+        } 
         return total / len;
     }
 }
