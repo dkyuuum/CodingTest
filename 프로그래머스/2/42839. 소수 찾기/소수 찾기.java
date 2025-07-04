09e3d37956512a6a5702b80cfc9563f3e69b1ac1
Kyumin <dkyuuum@gmail.com>
2025-07-04 09:10:05 +0900

    [level 2] Title: 소수 찾기, Time: 22.23 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,22 +1,23 @@
 import java.util.*;
 
 class Solution {
-    public Set<Integer> set = new HashSet<>();
+    HashSet<Integer> set = new HashSet<>();
     public int solution(String numbers) {
         int len = numbers.length();
-        backtracking(numbers, len, new boolean[len], "0");
+        
+        backTracking(numbers, "0", len, new boolean[len]);
         
         return set.size();
     }
     
-    public void backtracking(String numbers, int len, boolean[] visited, String make) {
+    public void backTracking(String numbers, String make, int len, boolean[] visited) {
         int num = Integer.parseInt(make);
         if (isPrime(num)) set.add(num);
         
         for (int i=0; i<len; i++) {
             if (!visited[i]) {
                 visited[i] = true;
-                backtracking(numbers, len, visited, make+numbers.charAt(i));
+                backTracking(numbers, make+numbers.charAt(i), len, visited);
                 visited[i] = false;
             }
         }
@@ -24,9 +25,11 @@ class Solution {
     
     public boolean isPrime(int n) {
         if (n == 0 || n == 1) return false;
+        
         for (int i=2; i*i<=n; i++) {
             if (n % i == 0) return false;
         }
+        
         return true;
     }
 }
