0c3e6fde7319fed156c77f3b970487b4f6881c75
Kyumin <dkyuuum@gmail.com>
2025-08-13 16:21:31 +0900

    [Gold IV] Title: 4연산, Time: 68 ms, Memory: 11688 KB -BaekjoonHub

--- 백준/Gold/14395. 4연산/4연산.java
+++ 백준/Gold/14395. 4연산/4연산.java
@@ -1,85 +1,80 @@
 import java.io .*;
-        import java.util .*;
+import java.util .*;
 
 public class Main {
-    static class Pair {
-        long num;
-        String op;
-
-        Pair(long num, String op) {
-            this.num = num;
-            this.op = op;
-        }
-    }
-
+   static class Pair {
+       long num;
+       String op;
+       
+       Pair(long num, String op) {
+           this.num = num;
+           this.op = op;
+       }
+   }
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
+        
         long s = Integer.parseInt(st.nextToken());
         long t = Integer.parseInt(st.nextToken());
         
-        // s == t
+        // s == t 
         if (s == t) {
             System.out.println(0);
             return;
         }
-
+        
         // 초기화
         Queue<Pair> queue = new LinkedList<>();
         HashSet<Long> visited = new HashSet<>();
-
+        
         queue.add(new Pair(s, ""));
         visited.add(s);
-
-        // BFS 탐색
-        while (!queue.isEmpty()) {
+        
+        while(!queue.isEmpty()) {
             Pair current = queue.poll();
-
             long n;
-            // *
+            
             n = current.num * current.num;
             if (n == t) {
-                System.out.println(current.op + "*");
+                System.out.println(current.op+"*");
                 return;
             }
-            if (n <= 1_000_000_000 && visited.add(n)) {
-                queue.add(new Pair(n, current.op + "*"));
+            if (n<=1_000_000_000 && visited.add(n)) {
+                queue.add(new Pair(n, current.op+"*"));
             }
-
-            // +
+            
             n = current.num + current.num;
             if (n == t) {
-                System.out.println(current.op + "+");
+                System.out.println(current.op+"+");
                 return;
             }
-            if (n <= 1_000_000_000 && visited.add(n)) {
-                queue.add(new Pair(n, current.op + "+"));
+            if (n<=1_000_000_000 && visited.add(n)) {
+                queue.add(new Pair(n, current.op+"+"));
             }
-
-            // -
+            
             n = current.num - current.num;
             if (n == t) {
-                System.out.println(current.op + "-");
+                System.out.println(current.op+"-");
                 return;
             }
             if (visited.add(n)) {
-                queue.add(new Pair(n, current.op + "-"));
+                queue.add(new Pair(n, current.op+"-"));
             }
-
-            // /
+            
             if (current.num != 0) {
+                
                 n = current.num / current.num;
-                if (n == t) {
-                    System.out.println(current.op + "/");
+               if (n == t) {
+                    System.out.println(current.op+"/");
                     return;
                 }
-
                 if (visited.add(n)) {
-                    queue.add(new Pair(n, current.op + "/"));
+                    queue.add(new Pair(n, current.op+"/"));
                 }
             }
         }
-        System.out.println(-1);
+                System.out.println(-1);
     }
 }
