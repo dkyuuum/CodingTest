53317e81d41211667c9e056896d35f98cdae6b09
Kyumin <dkyuuum@gmail.com>
2025-09-02 13:35:55 +0900

    [Gold IV] Title: 운동, Time: 560 ms, Memory: 58640 KB -BaekjoonHub

---
+++ 백준/Gold/1956. 운동/운동.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
+
+        final int INF = 1_000_000_000;
+        int[][] dist = new int[V+1][V+1];
+        for (int i = 1; i <= V; i++) {
+            Arrays.fill(dist[i], INF);
+            dist[i][i] = 0;
+        }
+
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            dist[a][b] = c;
+        }
+
+        // Floyd-Warshall 수행
+        for (int k = 1; k <= V; k++) {
+            for (int i = 1; i <= V; i++) {
+                for (int j = 1; j <= V; j++) {
+                    if (dist[i][j] > dist[i][k] + dist[k][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                    }
+                }
+            }
+        }
+
+        // 최단 사이클 거리 찾기
+        int answer = INF;
+        for (int i = 1; i <= V; i++) {
+            for (int j = 1; j <= V; j++) {
+                if (i != j && dist[i][j] < INF && dist[j][i] < INF) {
+                    answer = Math.min(answer, dist[i][j] + dist[j][i]);
+                }
+            }
+        }
+
+        System.out.println(answer == INF ? -1 : answer);
+    }
+}
