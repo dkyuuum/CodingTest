78c6219ca513b3506d861ec30410ddaac085cbaa
Kyumin <dkyuuum@gmail.com>
2025-12-08 23:51:25 +0900

    [Silver III] Title: 바이러스, Time: 88 ms, Memory: 9440 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.js
@@ -0,0 +1,36 @@
+/*
+const input = require('fs')
+  .readFileSync('example.txt')
+  .toString()
+  .trim()
+  .split('\n');
+  */
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+let n = Number(input.shift());
+let m = Number(input.shift());
+
+let graph = [...new Array(n + 1)].map(() => []);
+let visited = new Array(n + 1).fill(false);
+let ans = 0;
+
+visited[1] = true;
+
+const dfs = (start) => {
+  graph[start].map((dest) => {
+    if (!visited[dest]) {
+      visited[dest] = true;
+      ans += 1;
+      dfs(dest);
+    }
+  });
+};
+
+input.map((i) => {
+  const [start, dest] = i.split(' ').map((ele) => Number(ele));
+  graph[start].push(dest);
+  graph[dest].push(start);
+});
+
+dfs(1);
+
+console.log(ans);
