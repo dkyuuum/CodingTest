508585371bb2ab58a6428db97992d2ccb6db5a78
Kyumin <dkyuuum@gmail.com>
2026-04-06 13:21:48 +0900

    [level 3] Title: 네트워크, Time: 0.21 ms, Memory: 90.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,25 +1,25 @@
 class Solution {
+    public static boolean[] visited;
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        boolean[] visited = new boolean[n+1];
+        visited = new boolean[n];
         
-        
-        for (int i=0; i<n; i++) {
+        for (int i=0; i<computers.length; i++) {
             if (!visited[i]) {
+                dfs(i, computers, n);
                 answer++;
-                dfs(computers, visited, i);
             }
         }
         
         return answer;
     }
     
-    public static void dfs(int[][] computers, boolean[] visited, int i) {
+    public void dfs(int i, int[][] computers, int n) {
         visited[i] = true;
         
-        for (int j=0; j<computers.length; j++) {
-            if (!visited[j] && computers[i][j] == 1) { // 방문하지 않았고 값이 1이라면
-                dfs(computers, visited, j);
+        for (int j=0; j<n; j++) {
+            if (!visited[j] && computers[i][j] == 1) {
+                dfs(j, computers, n);
             }
         }
     }
