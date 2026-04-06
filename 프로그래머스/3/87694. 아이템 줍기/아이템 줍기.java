7419d1beb6b1d9d3b04e924b3cdecbcb217c5c42
Kyumin <dkyuuum@gmail.com>
2026-04-06 19:49:49 +0900

    [level 3] Title: 아이템 줍기, Time: 1.02 ms, Memory: 87.6 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
@@ -17,7 +17,7 @@ class Solution {
 
             for (int i = x1; i <= x2; i++) {
                 for (int j = y1; j <= y2; j++) {
-                    map[i][j] = 1;
+                    map[i][j] = 1;                          // 전체 값 채우기
                 }
             }
         }
@@ -28,16 +28,16 @@ class Solution {
             int x2 = r[2] * 2;
             int y2 = r[3] * 2;
 
-            for (int i = x1 + 1; i < x2; i++) {
+            for (int i = x1 + 1; i < x2; i++) {             
                 for (int j = y1 + 1; j < y2; j++) {
-                    map[i][j] = 0;
+                    map[i][j] = 0;                          // 테두리 제외하고 내용 없애기
                 }
             }
         }
         
         Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{characterX*2, characterY*2, 0});
-        visited[characterX*2][characterY*2] = true;
+        q.add(new int[]{characterX*2, characterY*2, 0});    // 시작 위치 * 2 (좌표값 키우기)
+        visited[characterX*2][characterY*2] = true;         // 시작 위치 방문 처리
         
         while(!q.isEmpty()) {
             int[] cur = q.poll();
@@ -46,17 +46,17 @@ class Solution {
             int dist = cur[2];
                         
             for (int i=0; i<4; i++) {
-                int mx = cx + dx[i];
-                int my = cy + dy[i];
+                int mx = cx + dx[i];                        // 현재 x좌표 이동
+                int my = cy + dy[i];                        // 현재 y좌표 이동
                 
-                if (map[mx][my] == 1 && !visited[mx][my]) {
-                    visited[mx][my] = true;
-                    q.add(new int[]{mx, my, dist+1});
+                if (map[mx][my] == 1 && !visited[mx][my]) { // 값이 1이고 방문하지 않은 경우 
+                    visited[mx][my] = true;                 // 현재 위치 방문 처리
+                    q.add(new int[]{mx, my, dist+1});       // 
                 }
             }
             
-            if (cx == itemX*2 && cy == itemY*2) {
-                return dist / 2;
+            if (cx == itemX*2 && cy == itemY*2) {           // 현재 위치가 itemX, itemY와 같다면
+                return dist / 2;                            // 거리 / 2 반환
             }
         }
         
