dfc4399f05eaf481df0e9ecdcc10d4bdf9219b25
Kyumin <dkyuuum@gmail.com>
2026-04-07 15:03:44 +0900

    [Silver III] Title: 바이러스, Time: 108 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,49 +1,46 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static int count = 0;
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        int N = Integer.parseInt(br.readLine()); // 컴퓨터의 수
-        int M = Integer.parseInt(br.readLine()); // 컴퓨터 쌍의 수
 
-        ArrayList<Integer>[] graph = new ArrayList[N+1]; // 정수 리스트를 담는 배열로 선언
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int computer = Integer.parseInt(st.nextToken());
 
-        for (int i=1; i<=N; i++) {
+        StringTokenizer st2 = new StringTokenizer(br.readLine());
+        int connection = Integer.parseInt(st2.nextToken());
+        
+        ArrayList<Integer>[] graph = new ArrayList[computer+1];
+        boolean[] visited = new boolean[computer+1];
+
+        for (int i=1; i<=computer; i++) {
             graph[i] = new ArrayList<>();
         }
 
-        for (int i=0; i<M; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
+        for (int i=0; i<connection; i++) {
+            StringTokenizer st3 = new StringTokenizer(br.readLine());
+            int num1 = Integer.parseInt(st3.nextToken());
+            int num2 = Integer.parseInt(st3.nextToken());
 
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-
-            graph[a].add(b);
-            graph[b].add(a);
+            graph[num1].add(num2);
+            graph[num2].add(num1);
         }
 
-        boolean[] visited = new boolean[N+1]; // 방문 노드 확인
-        int count = 0;
-        count = dfs(1, visited, graph, count);
-
-        System.out.println(count - 1);
+        dfs(visited, graph, 1);
+        System.out.println(count);
     }
-    public static int dfs(int v, boolean[] visited, ArrayList<Integer>[] graph, int count) {
-        visited[v] = true;
-        count++;
 
-        for (int i: graph[v]) {
-            if (!visited[i]) { // 방문하지 않은 노드로
-                count = dfs(i, visited, graph, count);
+    public static void dfs(boolean[] visited, ArrayList<Integer>[] graph, int node) {
+        visited[node] = true;
+
+        for (int next: graph[node]) {
+            if (!visited[next]) {
+                visited[next] = true;
+                count++;
+                dfs(visited, graph, next);
             }
         }
-
-        return count;
     }
-
 }
