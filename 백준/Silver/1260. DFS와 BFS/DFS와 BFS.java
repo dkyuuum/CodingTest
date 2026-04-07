3ed85653bbd369156f3be0c34dcd0d33d0b7ec6b
Kyumin <dkyuuum@gmail.com>
2026-04-07 15:40:56 +0900

    [Silver II] Title: DFS와 BFS, Time: 316 ms, Memory: 21388 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,77 +1,75 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st;
 
-        int N = Integer.parseInt(st.nextToken()); // 정점의 개수
-        int M = Integer.parseInt(st.nextToken()); // 간선의 개수
-        int V = Integer.parseInt(st.nextToken()); // 탐색을 시작할 정점의 번호
-
-        boolean[] visited; // 방문 노드 확인
-
-        ArrayList<Integer>[] graph = new ArrayList[N+1]; // 정수 리스트를 담는 배열로 선언
-//         List<List<Integer>> graph = new ArrayList<>(); // 2차원 리스트로 선언
+        st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+        
+        ArrayList<Integer>[] graph = new ArrayList[N+1];
+        boolean[] visited;
 
         for (int i=1; i<=N; i++) {
             graph[i] = new ArrayList<>();
         }
 
         for (int i=0; i<M; i++) {
-            // 간선이 연결하는 두 정점의 번호 입력
             st = new StringTokenizer(br.readLine(), " ");
 
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
 
-            graph[a].add(b);
-            graph[b].add(a);
+            graph[x].add(y);
+            graph[y].add(x);
         }
 
-//        Arrays.sort(graph);
-        // 번호가 작은 것부터 방문하도록 정렬
-        for (int i = 1; i <= N; i++) {
+        for (int i=1; i<=N; i++) {
             Collections.sort(graph[i]);
         }
 
-        visited = new boolean[N+1]; // 방문 노드 확인
-        dfs(V, visited, graph);
+        /**
+         * dfs
+         */
+        visited = new boolean[N+1];
+        dfs(visited, graph, V);
 
         System.out.println();
-
+        /**
+         * bfs
+         */
         visited = new boolean[N+1];
-        bfs(V, visited, graph);
+        bfs(visited, graph, V);
     }
 
-    public static void dfs(int v, boolean[] visited, ArrayList<Integer>[] graph) {
-        System.out.print(v + " ");
-        visited[v] = true;
+    public static void dfs(boolean[] visited, ArrayList<Integer>[] graph, int node) {
+        visited[node] = true;
+        System.out.print(node + " ");
 
-        for (int i: graph[v]) {
-            if (!visited[i]) { // 방문하지 않은 노드로
-                dfs(i, visited, graph);
+        for (int next: graph[node]) {
+            if (!visited[next]) {
+                dfs(visited, graph, next);
             }
         }
     }
 
+    public static void bfs(boolean[] visited, ArrayList<Integer>[] graph, int node) {
+        Queue<Integer> q = new LinkedList<>();
+        visited[node] = true;
+        q.add(node);
 
-    public static void bfs(int v, boolean[] visited, ArrayList<Integer>[] graph) {
-        ArrayDeque<Integer> deque = new ArrayDeque<>(10000);
-        deque.add(v);
-        visited[v] = true;
+        while(!q.isEmpty()) {
+            int cur = q.poll();
+            System.out.print(cur + " ");
 
-        while (!deque.isEmpty()) {
-            int node = deque.poll(); // 큐에 있는 값 가져오기
-            System.out.print(node + " ");
-
-            for (int i: graph[node]) {
-                if (!visited[i]) {
-                    visited[i] = true;
-                    deque.add(i);
+            for (int next: graph[cur]) {
+                if (!visited[next]) {
+                    visited[next] = true;
+                    q.add(next);
                 }
             }
         }
