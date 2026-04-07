81865df83d1082311408d9963695388633edebe2
Kyumin <dkyuuum@gmail.com>
2026-04-07 15:08:36 +0900

    [Silver III] Title: 바이러스, Time: 108 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -5,14 +5,12 @@ public class Main {
     public static int count = 0;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int computer = Integer.parseInt(st.nextToken());
-
-        StringTokenizer st2 = new StringTokenizer(br.readLine());
-        int connection = Integer.parseInt(st2.nextToken());
+        int computer = Integer.parseInt(br.readLine());             // 컴퓨터의 수
+        int connection = Integer.parseInt(br.readLine());           // 연결된 컴퓨터 쌍의 수
         
-        ArrayList<Integer>[] graph = new ArrayList[computer+1];
+        ArrayList<Integer>[] graph = new ArrayList[computer+1];     // 정수 리스트를 담는 배열로 선언
         boolean[] visited = new boolean[computer+1];
 
         for (int i=1; i<=computer; i++) {
@@ -20,9 +18,9 @@ public class Main {
         }
 
         for (int i=0; i<connection; i++) {
-            StringTokenizer st3 = new StringTokenizer(br.readLine());
-            int num1 = Integer.parseInt(st3.nextToken());
-            int num2 = Integer.parseInt(st3.nextToken());
+            st = new StringTokenizer(br.readLine(), " ");
+            int num1 = Integer.parseInt(st.nextToken());
+            int num2 = Integer.parseInt(st.nextToken());
 
             graph[num1].add(num2);
             graph[num2].add(num1);
@@ -33,11 +31,10 @@ public class Main {
     }
 
     public static void dfs(boolean[] visited, ArrayList<Integer>[] graph, int node) {
-        visited[node] = true;
+        visited[node] = true;               // 방문 처리
 
         for (int next: graph[node]) {
-            if (!visited[next]) {
-                visited[next] = true;
+            if (!visited[next]) {           // 방문하지 않은 노드의 경우
                 count++;
                 dfs(visited, graph, next);
             }
