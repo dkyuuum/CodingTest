c8827c5b17b34b6c919ca658944e3e0a54caf2ae
Kyumin <dkyuuum@gmail.com>
2026-04-08 10:47:47 +0900

    [Silver III] Title: 바이러스, Time: 104 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,42 +1,46 @@
 import java.io.*;
 import java.util.*;
 
+// 모든 경우의 수 -> dfs
 public class Main {
     public static int count = 0;
-    public static void main(String[] args) throws IOException{
+    
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-
-        int computer = Integer.parseInt(br.readLine());             // 컴퓨터의 수
-        int connection = Integer.parseInt(br.readLine());           // 연결된 컴퓨터 쌍의 수
         
-        ArrayList<Integer>[] graph = new ArrayList[computer+1];     // 정수 리스트를 담는 배열로 선언
-        boolean[] visited = new boolean[computer+1];
-
+        int computer = Integer.parseInt(br.readLine());            // 컴퓨터의 수
+        int connection = Integer.parseInt(br.readLine());          // 컴퓨터 쌍의 수
+        
+        ArrayList<Integer>[] graph = new ArrayList[computer+1];    // 그래프 초기화
         for (int i=1; i<=computer; i++) {
             graph[i] = new ArrayList<>();
         }
-
+        
         for (int i=0; i<connection; i++) {
             st = new StringTokenizer(br.readLine(), " ");
-            int num1 = Integer.parseInt(st.nextToken());
-            int num2 = Integer.parseInt(st.nextToken());
-
-            graph[num1].add(num2);
-            graph[num2].add(num1);
+            
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            
+            graph[n].add(m);
+            graph[m].add(n);
         }
-
-        dfs(visited, graph, 1);
+        
+        boolean[] visited = new boolean[computer+1];
+        visited[1] = true;
+        
+        dfs(graph, visited, 1);
         System.out.println(count);
     }
-
-    public static void dfs(boolean[] visited, ArrayList<Integer>[] graph, int node) {
-        visited[node] = true;               // 방문 처리
-
+    
+    public static void dfs(ArrayList<Integer>[] graph, boolean[] visited, int node) {
+        visited[node] = true;
+        
         for (int next: graph[node]) {
-            if (!visited[next]) {           // 방문하지 않은 노드의 경우
+            if (!visited[next]) {
+                dfs(graph, visited, next);
                 count++;
-                dfs(visited, graph, next);
             }
         }
     }
