3dc538ab851e3f16f6a674a1bfc9b7b3032b0cba
Kyumin <dkyuuum@gmail.com>
2026-04-08 10:24:08 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14668 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.util.*;
 
+// 최소 칸의 수 구하기 -> bfs
 public class Main {
     public static int[] dx = {1, -1, 0, 0};
     public static int[] dy = {0, 0, 1, -1};
@@ -8,40 +9,41 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        st = new StringTokenizer(br.readLine());
+        st = new StringTokenizer(br.readLine(), " ");
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-
-        int[][] road = new int[N][M];
+        
+        int[][] graph = new int[N][M];                // 미로 넣을 배열
         for (int i=0; i<N; i++) {
             String line = br.readLine();
-
+            
             for (int j=0; j<M; j++) {
-                road[i][j] = line.charAt(j) - '0';
+                graph[i][j] = line.charAt(j) -'0';
             }
         }
-
-        Queue<int[]> q = new LinkedList<>();
-        // road[0][0] = 1;
-        q.add(new int[]{0, 0});
+        
+        Queue<int[]> q = new LinkedList<>();            // bfs 할 Queue
+        q.add(new int[]{0, 0});                         // 첫번째 좌표 넣기 
+        graph[0][0] = 1;
 
         while(!q.isEmpty()) {
             int[] cur = q.poll();
             int cx = cur[0];
             int cy = cur[1];
-
+            
             for (int i=0; i<4; i++) {
                 int nx = cx + dx[i];
                 int ny = cy + dy[i];
-
+                
                 if (nx >= 0 && ny >= 0 && nx < N && ny < M) {
-                    if (road[nx][ny] == 1) {
-                        road[nx][ny] = road[cx][cy] + 1;
+                    if (graph[nx][ny] == 1) {
+                        graph[nx][ny] = 0;                    // 방문 처리
+                        graph[nx][ny] = graph[cx][cy] + 1;
                         q.add(new int[]{nx, ny});
-                    }
+                    } 
                 }
             }
         }
-        System.out.println(road[N-1][M-1]);
+        System.out.println(graph[N-1][M-1]);
     }
 }
