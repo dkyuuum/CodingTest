bfe7ed213f560fadff19012ebe8bc1f22284fd57
Kyumin <dkyuuum@gmail.com>
2026-04-18 23:06:23 +0900

    [level 2] Title: 올바른 괄호, Time: 20.56 ms, Memory: 55.7 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -3,11 +3,11 @@ import java.util.*;
 class Solution {
     boolean solution(String s) {
         Stack<Character> stack = new Stack<>();
-
-        for (char c : s.toCharArray()) {
+        
+        for (char c: s.toCharArray()) {
             if (c == '(') {
-                stack.push(c);
-            } else { // c == ')'
+                stack.push(')');
+            } else {
                 if (stack.isEmpty()) return false;
                 stack.pop();
             }
@@ -15,3 +15,4 @@ class Solution {
         return stack.isEmpty();
     }
 }
+
