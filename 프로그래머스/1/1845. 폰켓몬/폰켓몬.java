5e15e429992cc9e07d2d49a6670e4f90d45d17cb
Kyumin <dkyuuum@gmail.com>
2026-04-28 08:29:21 +0900

    [level 1] Title: 폰켓몬, Time: 5.44 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,16 +2,25 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
         int answer = 0;
-        HashSet<Integer> set = new HashSet<Integer>();
-        
         int len = nums.length;
-        int available = len / 2;
+        int can = len / 2;
         
-        for (int i=0; i<len; i++) {
-            set.add(nums[i]);
+        for (int num: nums) {
+            map.put(num, map.getOrDefault(num, 0)+1);
         }
         
-        return set.size() > available ? available : set.size();
+        /**
+        * 최대 몇 종류인지 고르는 문제
+        * map의 사이즈만으로 가능!
+        **/
+        if (map.size() > can) {
+            answer = can;
+        } else {
+            answer = map.size();
+        }
+        
+        return answer;
     }
 }
