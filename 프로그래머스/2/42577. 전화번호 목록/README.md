561f5a1b8664d1a54b594e70d87561f1019f1406
Kyumin <dkyuuum@gmail.com>
2026-04-28 08:58:45 +0900

    [level 2] Title: 전화번호 목록, Time: 281.88 ms, Memory: 217 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577) 
 
 ### 성능 요약
 
-메모리: 99.3 MB, 시간: 347.85 ms
+메모리: 217 MB, 시간: 281.88 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 28일 08:52:07
+2026년 04월 28일 08:58:42
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,13 +2,31 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        Arrays.sort(phone_book);
+//         Arrays.sort(phone_book);
         
-        for (int i=0; i<phone_book.length-1; i++) {
-            if (phone_book[i+1].startsWith(phone_book[i])) {
-                return false;
+//         for (int i=0; i<phone_book.length-1; i++) {
+//             if (phone_book[i+1].startsWith(phone_book[i])) {
+//                 return false;
+//             }
+//         }
+//         return true;
+        
+        /**
+        * HashSet 사용
+        **/
+        Set<String> set = new HashSet<>(Arrays.asList(phone_book));
+        
+        for (String number : phone_book) {
+            for (int i = 1; i < number.length(); i++) {
+                String prefix = number.substring(0, i);
+                
+                if (set.contains(prefix)) {
+                    return false;
+                }
             }
         }
+
         return true;
+        
     }
 }
